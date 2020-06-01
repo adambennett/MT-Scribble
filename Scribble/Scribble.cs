b2d3828254536fff24490cca7334c893c05a88f5
Adam Bennett <nyoxidetwitter@gmail.com>
2020-06-01 02:24:11 -0400

    init

---
+++ Scribble/Scribble.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Scribble
+{
+    [BepInPlugin("nyoxide.monstertrain.scribble", "Scribble", "1.0.0.0")]
+    [BepInProcess("MonsterTrain.exe")]
+    [BepInProcess("MtLinkHandler.exe")]
+    [BepInDependency("api.modding.train.monster")]
+    public class Scribbler : BaseUnityPlugin
+    {
+        private static ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource("Scribble");
+        
+        public static void log(LogLevel lvl, string msg)
+        {
+            logger.Log(lvl, msg);
+        }
+        
+        void Awake()
+        {
+            var harmony = new Harmony("nyoxide.monstertrain.harmony");
+            harmony.PatchAll();
+        }
+        
+    }
+}
---
+++ Scribble/ShowCardsPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace Scribble
+{
+    [HarmonyPatch(typeof(CompendiumSectionCards), "IsCardUnlockedAndDiscovered")]
+    class ShowAllCompendiumCardsPatch
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
